f3739779d298c250cb9eecb4a8d3c4f078ccac22
DavidGu <35072445+ChengWeiGu@users.noreply.github.com>
2025-06-23 14:08:55 +0800

    Update run_model.sh

--- ar_wind_farm_arima_exe_file/x86-setable-preds/run_model.sh
+++ ar_wind_farm_arima_exe_file/x86-setable-preds/run_model.sh
@@ -7,3 +7,7 @@ N_STEPS=25
 
 # ./run_model -m $MOD_FILE -i $INP_FILE -o $OUT_FILE -n $N_STEPS
 ./run_model --model=$MOD_FILE --input=$INP_FILE --output=$OUT_FILE --n_steps=$N_STEPS
+
+# 將預測資料拋到本機資料夾中
+# MNT_PATH="/mnt/d/Projects/Regression_train_inference/checkpoints/arima"
+# cp $OUT_FILE $MNT_PATH
